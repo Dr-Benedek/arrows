c485d2530a45eb5eb599835ae7e67cc943b07045
Benedek <66177741+Dr-Benedek@users.noreply.github.com>
2024-11-07 12:15:06 +0100

    5.óra (5-ös feladat.)

--- arrows.js
+++ arrows.js
@@ -1,31 +1,56 @@
 let totalPresses = 0;
 let correctPresses = 0;
+let totalReactionTime = 0;
 let directions = ['UP', 'DOWN', 'LEFT', 'RIGHT'];
 let currentDirection = '';
 let gameActive = false;
+let countdownTimer = 20;
+let countdownInterval;
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
-
 document.getElementById('start').addEventListener('click', startGame);
 document.getElementById('stop').addEventListener('click', stopGame);
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        startGame();
+    } else if (event.key === 'Backspace') {
+        stopGame();
+    }
+});
 
 function startGame() {
     totalPresses = 0;
     correctPresses = 0;
+    totalReactionTime = 0;
     gameActive = true;
+    countdownTimer = 20;
+
     updateScore();
     nextDirection();
+
+    countdownInterval = setInterval(() => {
+        countdownTimer--;
+        document.getElementById('timer').textContent =countdownTimer;
+        if (countdownTimer <= 0) {
+            stopGame();
+        }
+    }, 1000);
 }
 
 function stopGame() {
     gameActive = false;
+    clearInterval(countdownInterval);
     updateScore();
 }
+
+let reactionTime = 0;
 
 function nextDirection() {
     if (!gameActive) return;
 
     currentDirection = directions[Math.floor(Math.random() * directions.length)];
+    reactionTime = Date.now();
     document.getElementById('dph').textContent = currentDirection;
 }
 
@@ -43,19 +68,26 @@ document.addEventListener('keydown', async (event) => {
         if (userKey === currentDirection) {
             correctPresses++;
             document.getElementById(userKey).style.backgroundColor = "green";
-        }
-        else{
+            totalReactionTime += Date.now() - reactionTime;
+        } else {
             document.getElementById(userKey).style.backgroundColor = "red";
         }
+
         await sleep(200);
         ColorBack(userKey);
         nextDirection();
     }
 });
-function ColorBack(key){
-    document.getElementById(key).style.backgroundColor ='';
+
+function ColorBack(key) {
+    document.getElementById(key).style.backgroundColor = '';
 }
 
 function updateScore() {
-    document.getElementById('dph').textContent = `Összes: ${totalPresses}, Helyes: ${correctPresses}`;
+    if (totalPresses > 0) {
+        const averageReactionTime = (totalReactionTime / correctPresses).toFixed(3);
+        document.getElementById('dph').textContent = `Helyes: ${correctPresses}/${totalPresses}, Átlagos reakcióidő: ${averageReactionTime} ms`;
+    } else {
+        document.getElementById('dph').textContent = `Helyes: ${correctPresses}/${totalPresses}`;
+    }
 }
